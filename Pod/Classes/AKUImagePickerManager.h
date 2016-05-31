cb8b86c10de2e301203a6ebe985cf04e8efb74c5
akuraru <akuraru@gmail.com>
2016-05-31 15:50:34 +0900

    delete interface constraint

--- Pod/Classes/AKUImagePickerManager.h
+++ Pod/Classes/AKUImagePickerManager.h
@@ -19,16 +19,16 @@
 // Open the camera roll.
 // If the terminal where the camera does not have, and does not do anything.
 // If the camera is not allowed, it will show the alert. In the case of iOS8 later, it will prompt to open the setting.
-- (void)openCameraWithDelegate:(__weak id<AKUImageManagerProtocol>)this item:(UIBarButtonItem *)item view:(UIView *)view;
+- (void)openCameraWithDelegate:(__weak id)this item:(UIBarButtonItem *)item view:(UIView *)view;
 
 // It will open the Photo Album.
 // If the Photo Album is not allowed, it will show the alert. In the case of iOS8 later, it will prompt to open the setting.
-- (void)openPhotoAlbumWithDelegate:(__weak id<AKUImageManagerProtocol>)this item:(UIBarButtonItem *)item view:(UIView *)view;
+- (void)openPhotoAlbumWithDelegate:(__weak id)this item:(UIBarButtonItem *)item view:(UIView *)view;
 
 
 // Hide the ViewController that was displayed in openPickerController:view:imagePickerCtrl:.
-- (void)dismissPickerView:(__weak id<AKUImageManagerProtocol>)this;
+- (void)dismissPickerView:(__weak id)this;
 
 // View the UIActionSheet, and then select whether from camera or album to get a photo.
-- (void)selectViewToOpen:(__weak id<AKUImageManagerProtocol>)controller item:(UIBarButtonItem *)item view:(UIView *)view;
+- (void)selectViewToOpen:(__weak id)controller item:(UIBarButtonItem *)item view:(UIView *)view;
 @end
